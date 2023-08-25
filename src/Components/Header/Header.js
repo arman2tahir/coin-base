6e541d2c9fbbe568da990dabda9ac4fee1631616
arman2tahir <armaantahir786@gmail.com>
2023-08-25 15:11:29 +0500

    Header Completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import logo from "../../Images/Logo.svg";
+import SearchIcon from "../../Images/Search-Icon.svg";
+import Agent from "../../Images/Agent.png";
+import Bell from "../../Images/bell.svg";
+import './Header.css';
 
 export default function Header() {
   return (
-    <div>
-      hello
-    </div>
+    <header>
+      <nav>
+        <div className="left">
+          <img src={logo} alt="cionbase logo" className="logo" />
+        </div>
+        <div className="right">
+          <div className="searchbar">
+            <img src={SearchIcon} alt="Search Icon" className="s-icon" />
+            <div className="search">
+              <input type="search" placeholder="Search e.g cards" />
+            </div>
+          </div>
+          <div className="p-img">
+            <img src={Agent} alt="Img" className="n-img" />
+            <p className="img-name">Agent Rose</p>
+          </div>
+          <div className="bell-notification">
+            <img src={Bell} alt="Bell Icon" className="bell" />
+            <p className="b-num">24</p>
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
