6601b73122c17b769f23d563a7f7cea276509c5b
arman2tahir <armaantahir786@gmail.com>
2023-08-28 22:34:28 +0500

    Side Bar is now working properly with active class

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
 import Eye from "../../Images/overview.svg";
 import User from "../../Images/users.svg";
@@ -9,71 +9,42 @@ import Statics from "../../Images/stats.svg";
 import Settings from "../../Images/setting.svg";
 import { Link } from 'react-router-dom';
 
-import { useState } from "react";
 
 export default function SideBar() {
 
-  const sidebarItems = document.querySelectorAll('.s-overview ');
+  const [activeBar, setActiveBar] = useState(0);
+
+  const sidebarItems = [
+    { icon: Eye, label: "Overview", path: "/" },
+    { icon: User, label: "User", path: "/users" },
+    { icon: Message, label: "Trade", path: "/" },
+    { icon: Wallet, label: "Wallet", path: "/wallet" },
+    { icon: Reuse, label: "Transactions", path: "/transaction" },
+    { icon: Statics, label: "Statistics", path: "/statistics" },
+    { icon: Settings, label: "Setting", path: "/" },
+    
+  ];
+
+  const handleItemClick = (index) => {
+    setActiveBar(index);
+  };
 
   return (
     <aside>
       <div className="sidebar">
-        <Link to="/" >
-        <div className="s-overview active">
-          <div className="s-wrapper">
-            <img src={Eye} alt="Overview" className="s-eye" />
-            Overview
-          </div>
-        </div>
-        </Link>
-        <Link to="/users">
-        <div className="s-overview">
-          <div className="s-wrapper">
-            <img src={User} alt="User" className="s-eye" />
-            User
-          </div>
-        </div>
-        </Link>
-        <Link to="/">
-        <div className="s-overview">
-          <div className="s-wrapper">
-            <img src={Message} alt="Message" className="s-eye" />
-            Trade
-          </div>
-        </div>
-        </Link>
-        <Link to="/wallet">
-        <div className="s-overview">
-          <div className="s-wrapper">
-            <img src={Wallet} alt="Wallet" className="s-eye" />
-            Wallet
-          </div>
-        </div>
-        </Link>
-        <Link to="/transaction">
-        <div className="s-overview">
-          <div className="s-wrapper">
-            <img src={Reuse} alt="Reuse" className="s-eye" />
-            Transactions
-          </div>
-        </div>
-        </Link>
-        <Link to="/statistics">
-        <div className="s-overview">
-          <div className="s-wrapper">
-            <img src={Statics} alt="Statics" className="s-eye" />
-            Statistics
-          </div>
-        </div>
-        </Link>
-        <Link to="/">
-        <div className="s-overview">
-          <div className="s-wrapper">
-            <img src={Settings} alt="Settings" className="s-eye" />
-            Settings
-          </div>
-        </div>
-        </Link>
+      {sidebarItems.map((item, index) => (
+          <Link to={item.path} key={index}>
+            <div
+              className={`s-overview ${index === activeBar ? "active" : ""}`}
+              onClick={() => handleItemClick(index)}
+            >
+              <div className="s-wrapper">
+                <img src={item.icon} alt={item.label} className="s-eye" />
+                {item.label}
+              </div>
+            </div>
+          </Link>
+        ))}
         <div className="account-button">
           <button className="refer">Referral</button>
           <button className="logout">Logout</button>
